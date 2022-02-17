6521b3b0645d053e1989f63257766d7c18161a8c
Ershov Ilya <ershov64372@gmail.com>
2022-02-17 06:05:40 +0300

    Refactoring(IOVector): XML комментрий

--- Vector/scr/IOVector.h
+++ Vector/scr/IOVector.h
@@ -4,4 +4,8 @@
 
 #include <iostream>
 
+/// <summary>
+/// ¬ывод меню дл€ динамического массива
+/// </summary>
+/// <param name="vector">ƒинамический массив учавствующий в меню</param>
 void PrintVectorMenu(Vector& vector);
